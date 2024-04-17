590b93b5e77cef139ba5c01253f903c4d3a0f2ba
Katie Barrett <kayt.barrett22@gmail.com>
2024-04-17 15:45:03 -0600

    reads csv file, split each line to remove the commas, and print fields to console

--- CsvReader.cpp
+++ CsvReader.cpp
@@ -2,25 +2,42 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std; 
+
+vector<string> split(string s, char delim){
+    vector<string> tokens;
+    stringstream ss(s);
+    string token;
+
+    while(getline(ss, token, delim)){
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
 
 int main() {
 
     ifstream myfile;
     myfile.open("data.csv");
+    if (!myfile.is_open()){
+        cout << "Error opening file" << endl;
+        return 1;
+    }
 
-    string myline;
+    string line;
+    while (getline(myfile, line)){
+        vector<string> fields = split(line, ',');
+          
+        for (const auto &field : fields){
+        cout << field << " ";
+        }
+        cout << endl;
+    }
 
-    if (myfile.is_open()){
-        while (myfile){
-            getline (myfile, myline);
-            cout << myline << '\n';
-        }
-        myfile.close();
-    } else {
-        cout << "Couldn't open file";
-        }
+    myfile.close();
 
     return 0;
 
